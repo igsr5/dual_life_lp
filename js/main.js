6928273e8613565847a148b28bfa937ec82b92d2
Ichigo <ichigo@web-freework.com>
2020-09-17 11:50:45 +0900

    update: study-program部分まで完成

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(".slider").slick({
+    centerMode: true,
+    centerPadding: '5%',
+    dots: false,
+    autoplay: false,
+    arrows: false,
+    autoplaySpeed: 3000,
+    speed: 1000,
+    infinite: true,
+    variableWidth: true,
+});
